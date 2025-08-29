f26a6447c237027ea65c3f846aed89a0103d994d
kostabkpark2 <kostabkpark2@gmail.com>
2025-08-29 17:31:36 +0900

    JDBC - SQL CRUD

--- src/ch18/DBDemo3.java
+++ src/ch18/DBDemo3.java
@@ -2,36 +2,33 @@ package ch18;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
 
-public class DBDemo2 {
+public class DBDemo3 {
 
 	public static void main(String[] args) throws SQLException {
 		Connection conn = makeConnection();
-		// 3. statement 객체 생성
-		Statement stmt = conn.createStatement();
+		// 3. preparedStatement 인터페이스 사용
+		String sql = "delete from person where id = ?";
 
-		String sql = "select name, phone from person";
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		pstmt.setInt(1, 3);
+
 		// 4. sql 문 실행
-		ResultSet rs = stmt.executeQuery(sql);
-
-		while (rs.next()) {
-			System.out.println(rs.getString(1));
-			System.out.println(rs.getString(2));
+		if (pstmt.executeUpdate() == 1) {
+			System.out.println("레코드 삭제 성공");
+		} else {
+			System.out.println("레코드 삭제 실패");
 		}
-
 		// 5. 연결 종료
-		stmt.close();
+		pstmt.close();
 		conn.close();
 
 	}
 
 	public static Connection makeConnection() {
 		String url = "jdbc:mysql://localhost:3306/testdb?serverTimezone=Asia/Seoul";
-		// workbench -> home -> copy jdbc connection ~~~ :
-		// "jdbc:mysql://127.0.0.1:3306/?user=root"
 		Connection conn = null;
 
 		// 1. 드라이버 로드
