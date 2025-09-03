ddde13ad923e231139365eb9b68740b5da59c81b
kostabkpark2 <kostabkpark2@gmail.com>
2025-09-03 12:46:46 +0900

    madang database use

---
+++ src/madang/BookList.java
@@ -0,0 +1,27 @@
+package madang;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class BookList {
+	public static void main(String[] args) throws SQLException {
+		Connection conn = MakeConnection.getConnection();
+
+		String sql = "select * from book";
+		PreparedStatement stmt = conn.prepareStatement(sql);
+		ResultSet rs = stmt.executeQuery();
+
+		while (rs.next()) {
+			System.out.print(rs.getInt(1));
+			System.out.print(rs.getString(2));
+			System.out.print(rs.getString(3));
+			System.out.println(rs.getInt(4));
+		}
+
+		stmt.close();
+		conn.close();
+	}
+
+}
